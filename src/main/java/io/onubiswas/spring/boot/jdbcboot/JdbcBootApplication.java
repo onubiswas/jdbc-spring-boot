445f3d24a02d2b64c898b165e1bf529906880243
Anupama <anupama80286@gmail.com>
2024-02-16 00:16:51 +0530

    write code to query from database using JdbcTemplate

--- src/main/java/io/onubiswas/spring/boot/jdbcboot/JdbcBootApplication.java
+++ src/main/java/io/onubiswas/spring/boot/jdbcboot/JdbcBootApplication.java
@@ -1,13 +1,24 @@
 package io.onubiswas.spring.boot.jdbcboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class JdbcBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcBootApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+		String sql = "SELECT count(*) FROM T_ACCOUNT";
+		return args -> System.out.println("Hello, there are "
+						+ jdbcTemplate.queryForObject(sql, Long.class)
+						+ " accounts");
 	}
 
 }
--- src/test/java/io/onubiswas/spring/boot/jdbcboot/JdbcBootApplicationTests.java
+++ src/test/java/io/onubiswas/spring/boot/jdbcboot/JdbcBootApplicationTests.java
@@ -1,13 +1,23 @@
 package io.onubiswas.spring.boot.jdbcboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JdbcBootApplicationTests {
 
+	final String sql = "SELECT count(*) FROM T_ACCOUNT";
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 	@Test
-	void contextLoads() {
+	public void testNumberOfAccount() {
+		long count = jdbcTemplate.queryForObject(sql, Long.class);
+		assertThat(count).isEqualTo(21L);
 	}
 
 }
