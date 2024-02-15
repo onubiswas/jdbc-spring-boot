c85ac6326e236126b313ce6d1d15c6f88018f3cc
Anupama <anupama80286@gmail.com>
2024-02-16 00:03:27 +0530

    initial commit

---
+++ src/main/java/io/onubiswas/spring/boot/jdbcboot/JdbcBootApplication.java
@@ -0,0 +1,13 @@
+package io.onubiswas.spring.boot.jdbcboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBootApplication.class, args);
+	}
+
+}
